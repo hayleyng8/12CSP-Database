ce77ebb0132e141dec8ad332f144f71c1267ec5f
hayleyng8 <162944181+hayleyng8@users.noreply.github.com>
2024-07-24 00:59:28 +0000

    Trial Initial Commit

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
+import sqlite3
+from sqlite3 import Error
 
 app = Flask(__name__)
+DATABASE = "toydatabase.db"
 
-
-@app.route('/')
-def index():
-    return 'Hello from Flask!'
+def create_connection(db_file):
+    try:
+        connection = sqlite3.connect(db_file)
+        return connection
+    except Error as e:
+        print(e)
+    return None
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=5000)
+  app.run(host='0.0.0.0', port=81)
